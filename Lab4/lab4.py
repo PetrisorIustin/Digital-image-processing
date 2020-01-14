40980eb341d3c995ea317d404c28415da7d26709
iustin <iustinpetrisor@gmail.com>
2020-01-14 10:25:26 +0200

    'wherever'

---
+++ Lab4/lab4.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import glob
+import numpy as np
+from math import floor
+
+from skimage.feature import greycomatrix, greycoprops
+from skimage import data
+
+
+print(np.version.version)
